af9b301fbddfd61fc057be88d3c88eb71b2c03a4
Vikesh Kumar Verma <vikeshkm2002@gmail.com>
2026-04-13 07:46:56 +0530

    Add icons for all platform

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PIONEER RECUITMENT',
+        short_name: 'PIONEER',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: '/android/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/android/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          }]
+      }
+    })
+  ]
 })
